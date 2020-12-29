d28385d20be291140b365e8010aff251825bacd5
Saima Eva <69007708+Saima-Eva@users.noreply.github.com>
2020-12-29 12:01:20 +0600

    Add files via upload

---
+++ Number_Pattern.java
@@ -0,0 +1,17 @@
+
+package Numeric_Pattern;
+
+
+public class Number_Pattern {
+     public static void main(String[] args) {
+            int i, j, k = 1;
+            for (i = 1; i <= 5; i++) {
+                for (j = 1; j< i + 1; j++) {
+                    System.out.print(k++ + " ");
+                }
+       
+                System.out.println();
+    
+}
+     }
+}
